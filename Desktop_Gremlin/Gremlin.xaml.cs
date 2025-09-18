23ddc70a3ad379e31178d8310e86ae0be3c99c43
Kritzkingvoid <kurtfrancisgame5@gmail.com>
2025-09-18 22:25:19 +0800

    Updated Files

--- Desktop_Gremlin/Gremlin.xaml.cs
+++ Desktop_Gremlin/Gremlin.xaml.cs
@@ -29,10 +29,12 @@ namespace Desktop_Gremlin
         {
             public static int SpriteColumn { get; set; } = 10;
             public static int FrameRate { get; set; } = 31;
-            public static string StartingChar { get; set; } = "Mambo";
+            public static string StartingChar { get; set; } = "Haru";
             public static double FollowRadius { get; set; } = 150.0;
             public static int FrameWidth { get; set; } = 200;
             public static int FrameHeight { get; set; } = 200;
+
+            public static Dictionary<string, DateTime> LastPlayed = new Dictionary<string, DateTime>();
         }
         public class AnimationFrameCounts
         {
@@ -50,6 +52,7 @@ namespace Desktop_Gremlin
             public int Hover { get; set; } = 0;
             public int Sleep { get; set; } = 0; 
         }
+        
         public class AnimationFrame
         {
             public int Intro { get; set; } = 0;
@@ -68,7 +71,8 @@ namespace Desktop_Gremlin
         }
         public class State
         {
-            public bool PlayIntroOnNewSong { get; set; } = true;
+            //Some of the States are unused, I'll used them in the future
+            //If I need more percision when it comes to state managment
             public bool IsIntro { get; set; } = true;
             public bool ForwardAnimation { get; set; } = true;
             public bool IsRandom { get; set; } = false;
@@ -78,7 +82,6 @@ namespace Desktop_Gremlin
             public bool IsDragging { get; set; } = false;
             public bool IsWalkIdle { get; set; } = false;
             public bool IsClick { get; set; } = false;
-            public bool IsEmoting3 { get; set; } = false;
             public bool IsSleeping { get; set; } = false;   
         }
         public class MouseSettings
@@ -86,7 +89,6 @@ namespace Desktop_Gremlin
             public bool FollowCursor { get; set; } = false;
             public System.Drawing.Point LastMousePosition { get; set; }
             public double FollowSpeed { get; set; } = 5.0;
-            public DateTime LastCursorMove { get; set; }
             public double MouseX { get; set; }
             public double MouseY { get; set; }
             public double Speed { get; set; } = 10.0;
@@ -100,7 +102,6 @@ namespace Desktop_Gremlin
         private DispatcherTimer _closeTimer;
         private DispatcherTimer _masterTimer;
         private DispatcherTimer _idleTimer;
-        MediaPlayer player = new MediaPlayer();
         public struct POINT
         {
             public int X;
@@ -199,15 +200,6 @@ namespace Desktop_Gremlin
                         return null;
                 }
             }
-
-            public static void Unload(string animationName)
-            {
-                if (AlwaysCached.Contains(animationName.ToLower()))
-                {
-                    return;
-                }
-                Cache.Remove(animationName);
-            }
             public static void PruneCache()
             {
                 var keysToRemove = new List<string>();
@@ -225,7 +217,15 @@ namespace Desktop_Gremlin
                     Cache.Remove(key);
                 }
             }
-
+            public static void ClearCache()
+            {
+                foreach (var img in Cache.Values)
+                {
+                    img.StreamSource?.Dispose();
+                }
+                Cache.Clear();
+                _currentExtra = null;
+            }
             private static BitmapImage LoadSprite(string filefolder, string fileName, string rootFolder = "Gremlins")
             {
                 string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
@@ -250,28 +250,6 @@ namespace Desktop_Gremlin
                 }
             }
         }
-
-        //Former code where I just laod every sprite at once
-        //keep just in case
-        //private void LoadSpritesSheet()
-        //{
-        //    Sheets.Idle = LoadSprite(Settings.StartingChar, "idle.png");
-        //    Sheets.Intro = LoadSprite(Settings.StartingChar, "intro.png");
-        //    Sheets.WalkLeft = LoadSprite(Settings.StartingChar, "left.png");
-        //    Sheets.WalkRight = LoadSprite(Settings.StartingChar, "right.png");
-        //    Sheets.WalkDown = LoadSprite(Settings.StartingChar, "forward.png");
-        //    Sheets.WalkUp = LoadSprite(Settings.StartingChar, "backward.png");
-        //    Sheets.Outro = LoadSprite(Settings.StartingChar, "outro.png");
-        //    Sheets.Grab = LoadSprite(Settings.StartingChar, "grab.png");
-        //    Sheets.WalkIdle = LoadSprite(Settings.StartingChar, "wIdle.png");
-        //    Sheets.Click = LoadSprite(Settings.StartingChar, "click.png");
-        //    Sheets.Hover = LoadSprite(Settings.StartingChar, "hover.png");
-
-
-        //}
-
-        //Mainly for the user, This will be updated or debugging purposes
-        //To many edge cases for the program to crash.
         public static void FatalError(string message, string title = "Error")
         {
             System.Windows.MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
@@ -327,7 +305,6 @@ namespace Desktop_Gremlin
         }
         private void InitializeAnimations()
         {
-
             _masterTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1000.0 / Settings.FrameRate) };
             _masterTimer.Tick += (s, e) =>
             {
@@ -577,30 +554,63 @@ namespace Desktop_Gremlin
             menu.Items.Add(new ToolStripSeparator());
             menu.Items.Add("Reappear", null, (s, e) => ResetApp());
             menu.Items.Add("Close", null, (s, e) => CloseApp());
+            //List<string> options = new List<string>
+            //{
+            //    "Mambo",
+            //    "Haru",
+            //};
+            //var submenu = new ToolStripMenuItem("Options");
+            //foreach (var option in options)
+            //{
+            //    var item = new ToolStripMenuItem(option);
+            //    item.Click += (s, e) =>
+            //    {
+            //        Settings.StartingChar = option;
+            //        SpriteManager.ClearCache();
+            //        LoadConfigChar();
+            //        _masterTimer.Stop();
+            //        InitializeAnimations();
+            //    };
+            //    submenu.DropDownItems.Add(item);
+            //}
+
+            //menu.Items.Add(new ToolStripSeparator());
+            //menu.Items.Add(submenu);
 
             TRAY_ICON.ContextMenuStrip = menu;
         }
 
 
-        private void PlaySound(string fileName)
+        private void PlaySound(string fileName, double delaySeconds = 0)
         {
-            string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sounds", fileName);
+            string path = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Sounds", Settings.StartingChar, fileName);
 
             if (!File.Exists(path))
             {
                 return;
             }
-
+            if (delaySeconds > 0)
+            {
+                if (Settings.LastPlayed.TryGetValue(fileName, out DateTime lastTime))
+                {
+                    if ((DateTime.Now - lastTime).TotalSeconds < delaySeconds)
+                    {
+                        return;
+                    }
+                }
+            }
             try
             {
                 using (SoundPlayer sp = new SoundPlayer(path))
                 {
                     sp.Play();
                 }
+                Settings.LastPlayed[fileName] = DateTime.Now;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-             
             }
         }
 
@@ -767,9 +777,13 @@ namespace Desktop_Gremlin
 
         private void SpriteImage_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            if (!States.IsIntro)
+            if (!States.IsIntro && !States.IsWalking)
             {
                 States.IsHover = true;
+            }
+            if(!States.IsIntro && !States.IsWalking &&!States.IsSleeping &&!States.IsClick && !Mouse.FollowCursor)
+            {
+                PlaySound("hover.wav",3);
             }
         }
 
@@ -784,7 +798,6 @@ namespace Desktop_Gremlin
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
-            player.Close();
             TRAY_ICON?.Dispose();
         }
         private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
